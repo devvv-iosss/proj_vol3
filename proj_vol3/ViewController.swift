7b2af4683bb8600d23b66422051b5c96409c41ba
ios_dev <futureiosdeveloper@gmail.com>
2021-10-07 20:32:22 +0300

    Initial Commit

---
+++ proj_vol3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proj_vol3
+//
+//  Created by Vladimir Syleimanov on 07.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
